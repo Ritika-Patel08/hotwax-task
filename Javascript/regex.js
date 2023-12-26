cd9ee2d3e2cff11058a02535315827b266999a0d
Ritika-Patel08 <ritikapatel8870@gmail.com>
2023-12-26 20:35:21 +0530

    Implemented regular expression for name with letters

---
+++ Javascript/regex.js
@@ -0,0 +1,13 @@
+const regexName=/^[A-Za-z]+$/;
+
+const name1="Ritika";
+const name2="Ritika0808";
+const name3="112233ritika";
+const name4="Ritvi";
+
+console.log(regexName.test(name1));
+console.log(regexName.test(name2));
+console.log(regexName.test(name3));
+console.log(regexName.test(name4));
+
+
